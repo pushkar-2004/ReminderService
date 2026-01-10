bdd1a485a9af61ee3ee577ff0353aa2a640ca956
Pushkar Kumar <qwertypushkar123@gmail.com>
2026-01-10 23:28:06 +0530

    added email using nodemailer

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const {PORT} = require('./config/serverConfig');
+const {EMAIL,EMAI_PASSWORD} = require('./config/serverConfig');
+const {sendBasicEmail} = require('./services/email-services');
 
 function setUpAndStartServer(){
     const app = express();
@@ -9,6 +11,12 @@ function setUpAndStartServer(){
 
     app.listen(PORT,()=>{
         console.log(`Server started on port ${PORT}`);
+        sendBasicEmail(
+            'support@admin.com',
+            'qwertypushkar123@gmail.com',
+            'This is for testing',
+            'Hello from notification service demo '
+        );
     })
 
 }
---
+++ src/services/email-services.js
@@ -0,0 +1,18 @@
+const sender = require("../config/emailConfig");
+
+const sendBasicEmail = async (mailFrom, mailTo, mailSubject, mailBody) => {
+  try {
+    sender.sendMail({
+      from: mailFrom,
+      to: mailTo,
+      subject: mailSubject,
+      text: mailBody,
+    });
+  } catch (error) {
+    throw error;
+  }
+};
+
+module.exports = {
+  sendBasicEmail,
+};
